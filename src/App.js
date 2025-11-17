769f78e8d69a6e173d257383b2ec5e29409638d6
jontymahyavanshi <jontymahyavanshi@gmail.com>
2025-11-17 10:10:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function UserForm() {
   });
   const [users, setUsers] = useState([]);
   const [editingUser, setEditingUser] = useState(null);
-// npm -i -g json-server, j-s db.json --port 3001
+//npm install
+  // npm -i -g json-server, 
+  //json-server --watch db.json --port 3001
+  //npm start
   // Fetch all users when component loads
   useEffect(() => {
     fetchUsers();
